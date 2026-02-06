016660fe4f7f38add231053824bc645941ab85e4
ssss <kuffios@163.com>
2026-02-06 11:27:12 +0800

    大瓦房

--- include/ensq.hrl
+++ include/ensq.hrl
@@ -9,7 +9,7 @@
           short_id = undefined :: undefined | binary(),
           long_id = undefined :: undefined | binary(),
           feature_negotiation = false :: boolean(),
-          heartbeat_interval = -1 :: -1 | non_neg_integer(),
+          heartbeat_interval = 30000 :: non_neg_integer(),
           output_buffer_size = 16384 :: non_neg_integer(),
           output_buffer_timeout = 250 :: non_neg_integer(),
           tls_v1 = false :: boolean(),
